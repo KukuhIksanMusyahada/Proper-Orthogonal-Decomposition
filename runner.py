ee80a292b05045e40e8074f8ece7cd610b247079
Kukuh Iksan Musyahada <kukuh.iksan@gmail.com>
2022-07-01 00:41:05 +0700

    clean runner.py

--- runner.py
+++ runner.py
@@ -6,10 +6,10 @@ from POD_Lib import Calculation as calc
 
 if __name__=='__main__':
     sol_mat= calc.sol_matrix(names='CD')
-    print(sol_mat.shape)
+    print(f'Size of solution matrix is {sol_mat.shape}')
     U,s,V = calc.perform_SVD()
-    print(U.shape)
+    print(f'Size of U matrix from SVD is {U.shape}')
     k = calc.calc_K(s)
     U_hat = calc.calc_U_hat(U,k)
     delta_hat = calc.calc_delta_hat(U_hat)
-    print(delta_hat.shape)
+    print(f'Size of delta hat matrix is {delta_hat.shape}')
